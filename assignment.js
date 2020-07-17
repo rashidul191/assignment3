e6a1207f8cd5bbd58aec8cb638faa58043f96695
rashidul191 <67516291+rashidul191@users.noreply.github.com>
2020-07-17 11:02:04 +0600

    some code add

---
+++ assignment.js
@@ -0,0 +1,56 @@
+
+//........feetToMile........
+
+function feetToMile (number){
+    var feet = number/5280;
+    return feet;
+}
+// output and resutl.......
+var result = feetToMile(10245);
+result = result.toFixed(7);
+console.log(result);
+
+// ......end........
+
+
+
+//.............woodCalculator...........
+
+function woodCalculator(chare, table,khat){
+    chareCount = chare * 1;
+    tableCount = table * 2;
+    khatCount = khat * 5;
+    totalKat = chareCount + tableCount + khatCount;
+   return totalKat;
+}
+// output and resutl........
+var result = woodCalculator(15, 4, 10);
+console.log(result);
+
+// .........end..........
+
+
+
+// ..........brickCalculator...........
+
+function brickCalculator(number){
+    if(number >= 1 && number <= 10){
+
+        var result = ( number * ( 1000 * 15) );
+    }
+    else if(number >= 11 && number <= 20 ){
+        var result = ( 150000 + (number * ( 1000 *12) ) );
+               
+    }
+    else{
+        var result = ( 150000 + ( 120000 + ( number * ( 1000 * 10) ) ) );
+
+    }
+    return result ;
+}
+// output and resutl.....
+var floor =  brickCalculator(16);
+console.log(floor);
+
+// .........end..........
+
